fced9b53a828f0981d071c411925366ab1ef5a3b
rnd <community@vgp.rocks>
2017-03-08 00:35:19 -0700

    Small debugging modifications to helloworld.

---
+++ odin/plugins/helloworld/helloworld.py
@@ -0,0 +1,8 @@
+from errbot import BotPlugin, botcmd
+class HelloWorld(BotPlugin):
+    """Example 'Hello, world!' plugin for Errbot."""
+    
+    @botcmd
+    def hello(self, msg, args):
+        """Say hello to the world."""
+        return "Hello and welcome friend. Valhalla awaits you!"
