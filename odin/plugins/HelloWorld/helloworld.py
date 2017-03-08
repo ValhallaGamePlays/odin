109b2449062479e0c23039e1cbf0055b846efe7e
rnd <community@vgp.rocks>
2017-03-08 00:34:40 -0700

    added lots of stuff to .gitignore for common Python stuff

--- odin/plugins/HelloWorld/helloworld.py
+++
@@ -1,8 +0,0 @@
-from errbot import BotPlugin, botcmd
-class HelloWorld(BotPlugin):
-    """Example 'Hello, world!' plugin for Errbot."""
-    
-    @botcmd
-    def hello(self, msg, args):
-        """Say hello to the world."""
-        return "Hello and welcome friend. Valhalla awaits you!"
