0bec94e3b1dc4b50280100e6cc77c274f8a407e7
rnd <community@vgp.rocks>
2017-03-14 09:08:31 -0600

    Fix troll not working when trigger is prefixed or suffixed by punctuation.

--- odin/plugins/troll/troll.py
+++ odin/plugins/troll/troll.py
@@ -1,22 +1,27 @@
 from errbot import BotPlugin, re_botcmd
 import re
+import string
+
+
+def make_trigger(word):
+    return r"(^| |[" + string.punctuation + "])" + word + r"( |[" + string.punctuation + "]|$)"
 
 
 class TrollBot(BotPlugin):
 
-    @re_botcmd(pattern=r"(^| )cookie( |$)", prefixed=False, flags=re.IGNORECASE)
+    @re_botcmd(pattern=make_trigger("cookie"), prefixed=False, flags=re.IGNORECASE)
     def cookie(self, message, match):
         return 'What, you want a cookie!?'
 
-    @re_botcmd(pattern=r"(^| )complex doom( |$)", prefixed=False, flags=re.IGNORECASE)
+    @re_botcmd(pattern=make_trigger("complex doom"), prefixed=False, flags=re.IGNORECASE)
     def complex_doom(self, message, match):
         return 'Fuck Complex Doom!'
 
-    @re_botcmd(pattern=r"(^| )call of duty( |$)", prefixed=False, flags=re.IGNORECASE)
+    @re_botcmd(pattern=make_trigger("call of duty"), prefixed=False, flags=re.IGNORECASE)
     def call_of_duty(self, message, match):
         return 'Worst VG series ever BTW...'
 
-    @re_botcmd(pattern=r"(^| )shit( |$)", prefixed=False, flags=re.IGNORECASE)
+    @re_botcmd(pattern=make_trigger("shit"), prefixed=False, flags=re.IGNORECASE)
     def shit(self, message, match):
         return 'fuck'
 
